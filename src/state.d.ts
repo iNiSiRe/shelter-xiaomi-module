d6257cdb7836ccc2d254dc889725f999ad6b7d9e
inisire <inisire@gmail.com>
2023-09-04 19:03:36 +0300

    remove builded .js from repo

--- src/state.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-import { EventEmitter } from "events";
-export declare class ChangeSet extends Map<string, any> {
-}
-export declare class DeviceState<Scheme extends object> extends EventEmitter {
-    private readonly state;
-    update(changes: Scheme): void;
-    get props(): Scheme;
-}
